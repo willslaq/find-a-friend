ca5dd35f25cba173059c33d351b936704f59e27c
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-12-23 22:22:35 -0300

    chore: update on swagger script

--- updateSwaggerVersion.js
+++ updateSwaggerVersion.js
@@ -1,8 +1,12 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const pkg = await import('./package.json', {
-  assert: { type: 'json' }
-});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 
 const appFilePath = './src/app.ts';
 const appFileContent = fs.readFileSync(appFilePath, 'utf8');
