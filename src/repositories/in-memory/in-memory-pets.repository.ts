ff48b75638fe9452325b3ac3fe53338f9734b55f
Willinghan Tomaz <willslaqtomaz@gmail.com>
2025-01-08 22:09:49 -0300

    feat(pets): added new filter method to pets repository

--- src/repositories/in-memory/in-memory-pets.repository.ts
+++ src/repositories/in-memory/in-memory-pets.repository.ts
@@ -1,106 +1,141 @@
 import { Pet, Prisma } from "@prisma/client";
-import { PetsRepository } from "../pets.repository";
+import { FindManyParams, PetsRepository } from "../pets.repository";
 import { randomUUID } from "crypto";
 import { ResourceNotFoundError } from "@/services/errors/resource-not-found-error";
 
 export class InMemoryPetsRepository implements PetsRepository {
-    public items: Pet[] = []
+  public items: Pet[] = [];
 
-    async create(data: Prisma.PetCreateInput) {
-        const pet: Pet = {
-            id: randomUUID(),
-            age_in_month: data.age_in_month,
-            breed: data.breed,
-            name: data.name,
-            description: data.description,
-            species: data.species,
-            weight: data.weight,
-            created_at: new Date(),
-            is_available: false,
-            rescued_at: null,
-            updated_at: null,
-            deleted_at: null,
-            user_id: null
+  async create(data: Prisma.PetCreateInput) {
+    const pet: Pet = {
+      id: randomUUID(),
+      age_in_month: data.age_in_month,
+      breed: data.breed,
+      name: data.name,
+      description: data.description,
+      species: data.species,
+      weight: data.weight,
+      created_at: new Date(),
+      is_available: false,
+      rescued_at: null,
+      updated_at: null,
+      deleted_at: null,
+      user_id: null,
+    };
+
+    this.items.push(pet);
+
+    return pet;
+  }
+
+  async findById(id: string) {
+    const pet = this.items.find((item) => item.id === id);
+
+    if (!pet) {
+      return null;
+    }
+
+    return pet;
+  }
+
+  async findByUserId(userId: string) {
+    const pets = this.items.filter((item) => item.user_id === userId);
+
+    if (!pets) {
+      return null;
+    }
+
+    return pets;
+  }
+
+  async findMany({
+    species,
+    breed,
+    min_age,
+    max_age,
+    min_weight,
+    max_weight,
+    page = 1,
+    limit = 20,
+    sort = "asc",
+  }: FindManyParams) {
+    const filteredPets = this.items
+      .filter((item) => {
+        return (
+          (!species || item.species === species) &&
+          (!breed || item.breed === breed) &&
+          (!min_age || item.age_in_month >= min_age) &&
+          (!max_age || item.age_in_month <= max_age) &&
+          (!min_weight || item.weight >= min_weight) &&
+          (!max_weight || item.weight <= max_weight) &&
+          item.deleted_at === null
+        );
+      })
+      .sort((a, b) => {
+        if (sort === "asc") {
+          return a.created_at.getTime() - b.created_at.getTime();
+        } else {
+          return b.created_at.getTime() - a.created_at.getTime();
         }
+      })
+      .slice((page - 1) * limit, page * limit);
 
-        this.items.push(pet)
+    return filteredPets;
+  }
 
-        return pet
-    }
-    
-    async findById(id: string) {
-        const pet = this.items.find((item) => item.id === id)
+  async delete(id: string) {
+    const url = this.items.find((item) => item.id === id);
 
-        if (!pet) {
-            return null
-        }
-
-        return pet
+    if (!url) {
+      throw new ResourceNotFoundError();
     }
 
-    
-    async findByUserId(userId: string) {
-        const pets = this.items.filter((item) => item.user_id === userId)
+    url.deleted_at = new Date();
+  }
 
-        if (!pets) {
-            return null
-        }
+  async deleteAllByUserId(userId: string) {
+    this.items.forEach((item) => {
+      if (item.user_id === userId && !item.deleted_at) {
+        item.deleted_at = new Date();
+      }
+    });
+  }
 
-        return pets
-    }
-    
-    async delete(id: string) {
-        const url = this.items.find((item) => item.id === id)
+  async save(data: Pet) {
+    const index = this.items.findIndex((item) => item.id === data.id);
 
-        if (!url) {
-            throw new ResourceNotFoundError()
-        }
-
-        url.deleted_at = new Date()
+    if (index === -1) {
+      throw new ResourceNotFoundError();
     }
 
-    async deleteAllByUserId(userId: string) {
-        this.items.forEach((item) => {
-            if (item.user_id === userId && !item.deleted_at) {
-                item.deleted_at = new Date()
-            }
-        })
-    }
-    
-    async save(data: Pet) {
-        const index = this.items.findIndex((item) => item.id === data.id)
+    const existingPet = this.items[index];
 
-        if (index === -1) {
-            throw new ResourceNotFoundError()
-        }
+    const updatedPet: Pet = {
+      id: existingPet.id,
+      age_in_month: existingPet.age_in_month,
+      breed: existingPet.breed,
+      name: existingPet.name,
+      description: existingPet.description,
+      species: existingPet.species,
+      weight: existingPet.weight,
+      created_at: existingPet.created_at,
+      is_available: existingPet.is_available,
+      rescued_at: existingPet.rescued_at,
+      updated_at: new Date(),
+      deleted_at: existingPet.deleted_at,
+      user_id: existingPet.user_id,
+    };
 
-        const existingPet = this.items[index]
+    this.items[index] = updatedPet;
 
-        const updatedPet: Pet = {
-            id: existingPet.id,
-            age_in_month: existingPet.age_in_month,
-            breed: existingPet.breed,
-            name: existingPet.name,
-            description: existingPet.description,
-            species: existingPet.species,
-            weight: existingPet.weight,
-            created_at: existingPet.created_at,
-            is_available: existingPet.is_available,
-            rescued_at: existingPet.rescued_at,
-            updated_at: new Date(),
-            deleted_at: existingPet.deleted_at,
-            user_id: existingPet.user_id
-        }
+    return updatedPet;
+  }
 
-        this.items[index] = updatedPet
+  async countByUserId(userId: string) {
+    const count = this.items.filter(
+      (item) => item.user_id === userId && !item.deleted_at
+    ).length;
 
-        return updatedPet
-    }
-    
-    async countByUserId(userId: string) {
-        const count = this.items.filter((item) => item.user_id === userId && !item.deleted_at).length
-
-        return count
-    }
-
+    return count;
+  }
 }
--- src/repositories/pets.repository.ts
+++ src/repositories/pets.repository.ts
@@ -1,11 +1,24 @@
 import { Pet, Prisma } from "@prisma/client";
 
 export interface PetsRepository {
-    create(data: Prisma.PetCreateInput): Promise<Pet>;
-    findById(id: string): Promise<Pet | null>;
-    findByUserId(userId: string): Promise<Pet[] | null>;
-    deleteAllByUserId(userId: string): Promise<void>;
-    delete(id: string): Promise<void>;
-    save(data: Pet): Promise<Pet>;
-    countByUserId(userId: string): Promise<number>;
+  create(data: Prisma.PetCreateInput): Promise<Pet>;
+  findById(id: string): Promise<Pet | null>;
+  findByUserId(userId: string): Promise<Pet[] | null>;
+  deleteAllByUserId(userId: string): Promise<void>;
+  delete(id: string): Promise<void>;
+  save(data: Pet): Promise<Pet>;
+  countByUserId(userId: string): Promise<number>;
+  findMany(data: FindManyParams): Promise<Pet[]>;
+}
+
+export interface FindManyParams {
+  species?: string;
+  breed?: string;
+  min_age?: number;
+  max_age?: number;
+  min_weight?: number;
+  max_weight?: number;
+  page?: number;
+  limit?: number;
+  sort?: "asc" | "desc";
 }
